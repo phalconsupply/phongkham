5a1ae170ed5c029532f3875edd484893eea8a42f
Phalcon <dy01hoaquason@gmail.com>
2025-12-23 23:44:28 +0700

    Revert "fix: Add RedirectTenantUsers middleware to prevent tenant users accessing central routes"
    This reverts commit 198d05acf27460ed1f5330374d224166f19bd90e.

--- routes/web.php
+++ routes/web.php
@@ -18,9 +18,9 @@ Route::get('/', function () {
 
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified', \App\Http\Middleware\RedirectTenantUsers::class])->name('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware(['auth', \App\Http\Middleware\RedirectTenantUsers::class])->group(function () {
+Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
